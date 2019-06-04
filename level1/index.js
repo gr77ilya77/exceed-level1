e11b607de761bfe8663f500ca7350d8313a45c26
swarog0777 <ruslan1guchetl@gmail.com>
2019-06-04 13:56:53 +0300

    task6

--- level1/index.js
+++ level1/index.js
@@ -3,6 +3,17 @@
         task3();
         task4();
         task5();
+
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
     }
 
     document.addEventListener("DOMContentLoaded", ready);
